28ac1b37bc79729182224b24ca9b60fae285f599
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2018-10-21 16:40:13 -0400

    Redid exception handling

--- src/net/dhcpcord/backend/util/EntityChecker.java
+++ src/net/dhcpcord/backend/util/EntityChecker.java
@@ -3,7 +3,7 @@ package net.dhcpcord.backend.util;
 public class EntityChecker {
 
 	public static boolean isValid(String entity) {
-		for(Entities e : Entities.values()) {
+		for(Entitiy e : Entitiy.values()) {
 			if(e.toString().equals(entity)) {
 				return true;
 			}
